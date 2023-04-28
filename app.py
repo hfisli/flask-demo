7b9913023eaaffa69c91a038cbd5759599472247
hfisli <hfisli@adlere.fr>
2023-04-28 18:32:50 +0200

    Add book example

--- app.py
+++ app.py
@@ -1,29 +1,42 @@
-import mysql.connector
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
-db = mysql.connector.connect(
-    host="mysql",
-    user="user",
-    password="password",
-    database="mydb"
-)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://user:password@mysql/mydb'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80), nullable=False)
+    author = db.Column(db.String(80), nullable=False)
+
+@app.route('/books')
+def index():
+    books = Book.query.all()
+    return render_template('index.html', books=books)
+
+
+@app.route('/add_book', methods=['GET', 'POST'])
+def add_book():
+    if request.method == 'POST':
+        title = request.form['title']
+        author = request.form['author']
+        book = Book(title=title, author=author)
+        db.session.add(book)
+        db.session.commit()
+        return redirect(url_for('home'))
+    return render_template('add_book.html')
 
 @app.route('/')
-def index():
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM mytable")
-    data = cursor.fetchall()
-    return render_template('index.html', data=data)
+def home():
+    return render_template('home.html')
 
-@app.route('/add', methods=['POST'])
-def add():
-    cursor = db.cursor()
-    name = request.form['name']
-    value = request.form['value']
-    cursor.execute("INSERT INTO mytable (name, value) VALUES (%s, %s)", (name, value))
-    db.commit()
-    return redirect('/')
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port='8080', debug=True)
