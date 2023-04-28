84bbe46bd6d47e466f61fb7e5c1cf94c85b80be7
hfisli <hfisli@adlere.fr>
2023-04-28 18:06:01 +0200

    add DB connection

--- app.py
+++ app.py
@@ -1,14 +1,29 @@
-from flask import Flask, render_template
+import mysql.connector
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
+db = mysql.connector.connect(
+    host="mysql",
+    user="user",
+    password="password",
+    database="mydb"
+)
 
 @app.route('/')
-def home():
-    return render_template('home.html')
+def index():
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM mytable")
+    data = cursor.fetchall()
+    return render_template('index.html', data=data)
 
-@app.route('/about')
-def about():
-    return render_template('about.html')
+@app.route('/add', methods=['POST'])
+def add():
+    cursor = db.cursor()
+    name = request.form['name']
+    value = request.form['value']
+    cursor.execute("INSERT INTO mytable (name, value) VALUES (%s, %s)", (name, value))
+    db.commit()
+    return redirect('/')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='8080', debug=True)
+    app.run(host='0.0.0.0', port=8080)
