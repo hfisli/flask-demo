5d985feb54b4dea5df7bb17ae0eaeb89cb7a54bb
hfisli <131687851+hfisli@users.noreply.github.com>
2023-05-17 15:47:25 +0200

    Update app.py

--- app.py
+++ app.py
@@ -13,8 +13,8 @@ db_host = environ['MYSQL_HOST']
 db_name = environ['MYSQL_DATABASE']
 app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://{db_user}:{db_password}@{db_host}/{db_name}'
 
-
 db = SQLAlchemy(app)
+db.init_app(app)
 
 class Book(db.Model):
     id = db.Column(db.Integer, primary_key=True)
